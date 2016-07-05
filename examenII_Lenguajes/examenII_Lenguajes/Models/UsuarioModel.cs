00a223455d185752ea87326d40f31e3567c347dd
Jordy Garcia Mendoza <jordy0914@hotmail.com>
2016-07-05 15:30:21 -0600

    pruebas

---
+++ examenII_Lenguajes/examenII_Lenguajes/Models/UsuarioModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace examenII_Lenguajes.Models
+{
+    public class UsuarioModel
+    {
+        public int idCurso { get; set; }
+        public string nombre { get; set; }
+        public int creditos { get; set; }
+        public string descripcion { get; set; }
+
+    }
+
+
+    public class EstudianteDBContext : DbContext
+    {
+        public DbSet<UsuarioModel> UsuarioModel { get; set; }
+    }
+}
